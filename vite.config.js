ab3b6402e87bf34fa7da9eded2e3eb0e7e2b53ea
Maria Aftab <mkiyani012@gmail.com>
2025-08-23 23:52:54 +0500

    updated versions

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'your-repo-name' with your actual repository name
   base: '/my_portfolio_website/',
   build: {
     outDir: 'dist'
