966b8faad829a6d3355a7fb1e63f209001edd5c5
Maria Aftab <mkiyani012@gmail.com>
2025-08-23 23:48:59 +0500

    fixed workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your GitHub repository name
+  // Replace 'your-repo-name' with your actual repository name
+  base: '/my_portfolio_website/',
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 })
